561275186afc1d2e1dac840703b48d8f020160b0
bocacode <bocacode@bocacodes-MacBook-Air.local>
2021-07-15 10:47:45 -0400

    connected to mongo

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')// import express 
+const cors = require ('cors') // import cors
+const mongoose = require('mongoose')// import mongoose 
+require('dotenv/config') // importing dotenv library to use variable 
+
+const app = express()// creating app as express
+app.use(express.json()) // use express and parse everything into json
+
+mongoose
+.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true})// connect to mongoose 
+.then(() => console.log('connected to mongo...'))
+.catch(err => console.log(err))
+
+//first get route
+app.get('/', (req, res) => {
+  res.send('Hello Class')
+})
+
+
+app.listen(5000, (req, res) => {
+  console.log('Listening on port 5000.......')
+})
+
+
+
